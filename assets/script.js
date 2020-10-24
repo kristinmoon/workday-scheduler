bd209e9015a321cbab600f0ac5b8ffced4f8ea08
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2020-10-24 13:39:06 -0700

    added timestamps to left column

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,37 @@
 currentDay = $("#currentDay")
   .text(moment().format("[Today is] dddd, MMMM Do YYYY"));
 //console.log(currentDay);
+
+// set timestamps
+eightOclock = $("#eightOclock")
+  .text(moment().hour(8).minute(0).format("h:mm A"));
+
+eightOclock = $("#nineOclock")
+  .text(moment().hour(9).minute(0).format("h:mm A"));
+
+eightOclock = $("#tenOclock")
+  .text(moment().hour(10).minute(0).format("h:mm A"));
+
+eightOclock = $("#elevenOclock")
+  .text(moment().hour(11).minute(0).format("h:mm A"));
+
+eightOclock = $("#twelveOclock")
+  .text(moment().hour(12).minute(0).format("h:mm A"));
+
+eightOclock = $("#oneOclock")
+  .text(moment().hour(13).minute(0).format("h:mm A"));
+
+eightOclock = $("#twoOclock")
+  .text(moment().hour(14).minute(0).format("h:mm A"));
+
+eightOclock = $("#threeOclock")
+  .text(moment().hour(15).minute(0).format("h:mm A"));
+
+eightOclock = $("#fourOclock")
+  .text(moment().hour(16).minute(0).format("h:mm A"));
+
+eightOclock = $("#fiveOclock")
+  .text(moment().hour(17).minute(0).format("h:mm A"));
+
+
+var tasks = {}
